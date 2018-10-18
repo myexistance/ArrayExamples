d402868ed28f51adc19a79238ec23cf98c7cffdf
Hojoong Yoon <932400976@bun2-206-15.local>
2018-10-18 13:45:27 +1300

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// console.log("working");
+// alert("working");
+
+// var list = [];
+// console.log(list);
+// var question = prompt("What is the item you want to include?");
+// console.log(question);
+// list.push("Pear");
+// console.log(list);
+var list = [];
+function addNewItem(){
+	
+	console.log(list);
+	
+
+	
+	var question = prompt("What is the item you want to include?");
+	console.log(question);
+	list.push(question);
+	console.log(list);
+	//way number 1
+	// document.getElementById("shoppingList").innerHTML += "<li>"+question+"</li>";
+    //way with loop
+	for (var i = 0; i < list.length; i++) {
+		document.getElementById("shoppingList").innerHTML += "<li>"+list[i]+"</li>";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
